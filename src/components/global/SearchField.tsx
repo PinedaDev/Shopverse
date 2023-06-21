379a1bca5576e3e560f1f3e53ea121009ff39771
abraham <abraham@pinedadev.com>
2023-06-21 19:30:42 +0300

    fixed search field focus style

--- src/components/global/SearchField.tsx
+++ src/components/global/SearchField.tsx
@@ -14,9 +14,11 @@ const SearchField = ({ search, setSearch }: SearchField) => {
     <div
       className="relative flex border-2 border-gray-400 w-4/5 rounded-full 
       m-auto md:absolute md:top-4 md:left-2/4 md:-translate-x-2/4 lg:w-4/12 ">
-      <Icon iconRef="mdi-magnify" />
+      <span className="absolute top-2/4 -translate-y-2/4 translate-x-2/4">
+        <Icon iconRef="mdi-magnify" />
+      </span>
       <input
-        className="bg-transparent text-gray-200 text-center w-full rounded-full"
+        className="bg-transparent min-h-[2.5rem] text-gray-200 text-center w-full rounded-full"
         type="text"
         value={search.searchValue}
         onChange={searchHandler}
